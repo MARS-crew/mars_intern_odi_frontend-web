3fc28a071a5b1b964645b06270633373f5553755
pinomaker-hoo <inhoo23@naver.com>
2023-04-13 22:08:03 +0900

    feat : Set Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+// ** Router Imports
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 const App = () => {
-  return <h1>Hello world</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/login" element={<h1>Login Page</h1>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
