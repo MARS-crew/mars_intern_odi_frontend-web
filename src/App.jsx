5fcc547daa8e2cd9185a63aa15920821d5b26fde
pinomaker-hoo <inhoo23@naver.com>
2023-04-13 22:13:29 +0900

    feat : Set Script pretter and run format

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // ** Router Imports
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 const App = () => {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 // ** React Imports
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from "react-dom/client"
+import App from "./App"
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
