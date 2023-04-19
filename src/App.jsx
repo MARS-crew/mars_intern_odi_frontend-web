1edde66d23e825bf746a040eb7553507ec660583
HoYeon Jo <akftjd100@gmail.com>
2023-04-19 23:04:55 +0900

    [생성]1주차 과제 회원가입, 로그인 구현 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 // ** Router Imports
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-
+import "./App.css"
 import HomePage from "./pages/page-home"
 import LoginPage from "./pages/page-login"
+import RegisterPage from "./pages/page-register"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/page-home/index.jsx
+++ src/pages/page-home/index.jsx
@@ -1,5 +1,84 @@
-const HomePage = () => {
-  return <h1>Home Page</h1>
+function LandingPage() {
+  const style = {
+    container: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+      height: "100vh",
+      backgroundColor: "#E3AB9A",
+    },
+    mainContainer: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: "20px",
+      boxShadow: "10px 15px 100px #AD5F47",
+      padding: "20px",
+    },
+    mainLogoImg: {
+      display: "flex",
+      width: "380px",
+      height: "380px",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: "80px",
+      paddingRight: "20px",
+    },
+    mainView: {
+      backgroundColor: "white",
+      display: "flex",
+      flexDirection: "column",
+      width: "400px",
+      height: "400px",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: "20px",
+    },
+    yellowBtn: {
+      backgroundColor: "#F6F6E9",
+      borderColor: "#FCC944",
+      color: "#7B3911",
+      marginTop: "10px",
+      marginBottom: "15px",
+      borderRadius: "10px",
+      textAlign: "center",
+      fontSize: "20px",
+      fontWeight: "bold",
+      width: "350px",
+      height: "60px",
+    },
+    smallBtn: {
+      border: "none",
+      backgroundColor: "transparent",
+    },
+    textCenter: {
+      textAlign: "center",
+    },
+  }
+
+  return (
+    <div style={style.container}>
+      <div style={style.mainContainer}>
+        <img src="./src/assets/retro_logo.png" style={style.mainLogoImg}></img>
+        <div style={style.mainView}>
+          <h2>Welcome! HODI STUDIO</h2>
+          <h5 style={style.textCenter}>
+            This site provides information on the <br></br>history and works of
+            our studio. <br></br>Let's talk together! I hope you have a good
+            time.
+          </h5>
+          <button style={style.yellowBtn}>Site Go</button>
+          <button style={style.yellowBtn}>Inquiries about lease</button>
+          <div>
+            <button style={style.smallBtn}>로그인</button>
+            <span>|</span>
+            <button style={style.smallBtn}>회원가입</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default HomePage
+export default LandingPage
