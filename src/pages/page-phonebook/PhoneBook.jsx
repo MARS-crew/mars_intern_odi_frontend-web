c67f8c9a1b9ca5d28bfabf09d511acc984c44fe5
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-05-22 17:39:54 +0900

    fix : Add Authorization Option

--- src/pages/page-phonebook/PhoneBook.jsx
+++ src/pages/page-phonebook/PhoneBook.jsx
@@ -1,110 +1,113 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import vaildation from './PhoneBookValidation';
-import View from './View';
-import axios from 'axios';
+import React, { useState, useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import vaildation from "./PhoneBookValidation"
+import View from "./View"
+import axios from "axios"
 
 function PhoneBook() {
-  const [values, setValues] = useState([]);
-  const navigate = useNavigate();
-  const [errors, seterrors] = useState({});
+  const [values, setValues] = useState([])
+  const navigate = useNavigate()
+  const [errors, seterrors] = useState({})
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://phone.pinodev.shop:8000/api/phone');
-        setValues(response.data);
+        const response = await axios.get(
+          "http://phone.pinodev.shop:8000/api/phone",
+          {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          },
+        )
+        setValues(response.data)
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
-    };
+    }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   const handleInput = (e) => {
-    setValues((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
+    setValues((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+  }
 
   const handleSubmit = () => {
-    seterrors(vaildation(values));
+    seterrors(vaildation(values))
     if (errors.name === "" && errors.number === "") {
       axios
-        .post('http://phone.pinodev.shop:8000/api/phone',
-          {
-            name: values.name,
-            number: values.number
-          })
+        .post("http://phone.pinodev.shop:8000/api/phone", {
+          name: values.name,
+          number: values.number,
+        })
         .then((res) => {
-            navigate('/phonebook');
+          navigate("/phonebook")
         })
         .catch((err) => {
-          console.log(err.response);
-        });
+          console.log(err.response)
+        })
     }
-  };
-
+  }
 
   const deletenum = () => {
     axios
       .delete(`http://phone.pinodev.shop:8000/api/phone/${idx}`)
       .then((res) => {
-        setValues(...prev => prev.filter(value => value.idx !== idx));
+        setValues(...(prev) => prev.filter((value) => value.idx !== idx))
       })
       .catch((err) => {
-        console.log(err.response);
-      });
-  };
+        console.log(err.response)
+      })
+  }
 
   return (
-    <div className='wrapper'>
+    <div className="wrapper">
       <h1>PhoneBook</h1>
       <p>Add Friends</p>
-      <div className='main'>
-        <div className='form-container'>
-          <div className='form-group'>
-            <label htmlFor='name'>
+      <div className="main">
+        <div className="form-container">
+          <div className="form-group">
+            <label htmlFor="name">
               <strong>Name</strong>
             </label>
             <br />
             <input
-              type='name'
-              className='form-control'
-              name='name'
-              placeholder='Enter Name'
+              type="name"
+              className="form-control"
+              name="name"
+              placeholder="Enter Name"
               onChange={handleInput}
             />
-            {errors.name && <span id='text-danger'>{errors.name}</span>}
+            {errors.name && <span id="text-danger">{errors.name}</span>}
             <br />
             <br />
-            <label htmlFor='number'>
+            <label htmlFor="number">
               <strong>Number</strong>
             </label>
             <br />
             <input
-              className='form-control'
-              placeholder='Enter Number'
-              name='number'
+              className="form-control"
+              placeholder="Enter Number"
+              name="number"
               onChange={handleInput}
             />
-            {errors.number && (
-              <span number='text-danger'>{errors.number}</span>
-            )}
+            {errors.number && <span number="text-danger">{errors.number}</span>}
             <br />
             <br />
             <button
-              className='btn btn-success btn-md'
-              type='submit'
+              className="btn btn-success btn-md"
+              type="submit"
               onClick={handleSubmit}
             >
               ADD
             </button>
           </div>
         </div>
-        <div className='view-container'>
+        <div className="view-container">
           {values.length > 0 ? (
-            <div className='table-responsive'>
-              <table className='table'>
+            <div className="table-responsive">
+              <table className="table">
                 <thead>
                   <tr>
                     <th>Name</th>
@@ -118,7 +121,9 @@ function PhoneBook() {
                       <td>{item.name}</td>
                       <td>{item.number}</td>
                       <td>
-                        <button onClick={() => deletenum(item.idx)}>Delete</button>
+                        <button onClick={() => deletenum(item.idx)}>
+                          Delete
+                        </button>
                       </td>
                     </tr>
                   ))}
@@ -131,7 +136,7 @@ function PhoneBook() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default PhoneBook;
+export default PhoneBook
